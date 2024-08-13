68f62faafcfa2d66b37b1d25e395b44a002e8924
alona <aloyna.vasilieva@gmail.com>
2024-08-13 12:00:11 +0300

    fixed

--- Vasylieva Alona/task-9.js
+++ Vasylieva Alona/task-9.js
@@ -1,8 +1,8 @@
 db.users.updateMany(
   {
-    country: { $ne: "France" },
+    country: { $ne: "China" },
     is_blocked: { $ne: true },
-    balance: { $lt: 0 },
+    balance: { $gt: 0 },
   },
   {
     $mul: { balance: 1.155 },
